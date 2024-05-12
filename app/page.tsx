62df3564e43cd0b2370b23cc90186d18ad2bed4c
Julian Silva da Cunha <juliancunha2010@hotmail.com>
2024-05-12 11:36:35 -0300

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,217 @@
+"use client";
 import Image from "next/image";
+import { ToggleGroup, ToggleGroupItem } from "@/components/ui/toggle-group";
+import {
+  AppWindow,
+  Bold,
+  BugPlay,
+  Columns2,
+  Copy,
+  Download,
+  Italic,
+  Play,
+  RotateCw,
+  Rows2,
+  Terminal,
+  Underline,
+} from "lucide-react";
+import { ModeToggle } from "@/components/mode-toggle";
+import { Separator } from "@/components/ui/separator";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+
+import Editor from "@monaco-editor/react";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "@/components/ui/resizable";
+import { Button } from "@/components/ui/button";
+
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import { code, terminal_code } from "../public/example-code";
+import { Input } from "@/components/ui/input";
+import * as React from "react";
 
 export default function Home() {
+  const [terminal, setTerminal] = React.useState(false);
+  const [fullview, setFullview] = React.useState(false);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="relative flex flex-col bg-background ">
+      <header className="sticky top-0 z-50 w-full border-b border-border/40 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+        <div className="container flex h-14 max-w-screen-2xl items-center">
+          <div className="mr-4">
+            <a href="" className="font-bold flex items-center">
+              <BugPlay className="w-5 h-5 mr-2" />
+              Elixir Repl
+            </a>
+          </div>
+          <div className="ml-auto flex items-center">
+            <ToggleGroup variant="outline" type="single">
+              {/* <ToggleGroupItem value="columns-2" aria-label="Toggle Columns 2">
+                <Columns2 className="h-4 w-4" />
+              </ToggleGroupItem>
+              <ToggleGroupItem value="rows-2" aria-label="Toggle Rows">
+                <Rows2 className="h-4 w-4" />
+              </ToggleGroupItem> */}
+              <ToggleGroupItem
+                value="fullpage"
+                aria-label="Toggle fullpage"
+                onClick={() => setFullview(!fullview)}
+              >
+                <AppWindow className="h-4 w-4" />
+              </ToggleGroupItem>
+            </ToggleGroup>
+            <Separator orientation="vertical" className="!h-8 mx-4 block" />
+
+            <ModeToggle />
+          </div>
         </div>
-      </div>
+      </header>
+      <main
+        className={`custom-screen-height grid  ${
+          fullview ? "grid-cols-1" : "grid-cols-2"
+        }`}
+      >
+        {!fullview && (
+          <div className="h-full relative rounded">
+            <div className="p-2 flex gap-2 ">
+              <Button>
+                {" "}
+                <Play className="w-5 h-5 mr-2" /> Run{" "}
+              </Button>
+              <div className="ml-auto flex gap-2">
+                <Button variant={"secondary"}>
+                  {" "}
+                  <Copy className="w-4 h-4 mr-2" /> Copy{" "}
+                </Button>
+                <Button variant={"secondary"}>
+                  {" "}
+                  <Download className="w-4 h-4 mr-2" /> Download{" "}
+                </Button>
+              </div>
+            </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+            <div className="custom-editor-height px-2 pb-2">
+              <Editor
+                theme="vs-dark"
+                className="!rounded-md"
+                defaultLanguage="javascript"
+                defaultValue={code}
+              />
+            </div>
+          </div>
+        )}
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+        <div className="h-full relative">
+          <div className="p-2 flex flex-col bg-slate-200 dark:bg-slate-900 rounded h-full">
+            <div className="flex gap-2 mb-2">
+              <Button variant={"secondary"} size={"icon"} className="px-2">
+                <RotateCw className="w-5 h-5" />
+              </Button>
+              <Input defaultValue="https://ultimatemercer.com/" />
+              <Button
+                variant="secondary"
+                size={"icon"}
+                className="px-2"
+                onClick={() => setTerminal(!terminal)}
+              >
+                <Terminal className="w-5 h-5" />
+              </Button>
+            </div>
+            <iframe
+              src="https://ultimatemercer.com/"
+              width={"100%"}
+              height={"100%"}
+              className="rounded-md"
+            ></iframe>
+            {terminal && (
+              <div className="my-2">
+                <h4 className="">
+                  <span className="px-2.5 py-1 rounded-t-md bg-black text-white text-lg font-bold inline-flex items-center">
+                    <Terminal className="!w-4 !h-4 mr-2" />
+                    Logs
+                  </span>
+                </h4>
+                <ScrollArea className="h-[300px] w-full rounded-md p-4 bg-black text-cyan-500 font-mono rounded-tl-none">
+                  <pre>
+                    <code>{terminal_code}</code>
+                  </pre>
+                </ScrollArea>
+              </div>
+            )}
+          </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          {/* <ResizablePanelGroup direction="vertical">
+            <ResizablePanel defaultSize={85}>
+              <iframe
+                src="https://ultimatemercer.com/"
+                width={"100%"}
+                height={"100%"}
+              ></iframe>
+            </ResizablePanel>
+            <ResizableHandle />
+            <ResizablePanel
+              defaultSize={15}
+              className="bg-slate-800 px-2 py-4 text-cyan-500 font-mono"
+            >
+              <pre>
+                <code>{terminal_code}</code>
+              </pre>
+            </ResizablePanel>
+          </ResizablePanelGroup> */}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+          <Drawer>
+            <DrawerTrigger asChild>
+              <Button
+                variant="secondary"
+                className="absolute bottom-4 right-6 dark:border dark:border-white"
+              >
+                <Terminal className="w-4 h-4 mr-2" /> Logs
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent>
+              <DrawerHeader>
+                <DrawerTitle className="flex items-center mb-2">
+                  <Terminal className="w-5 h-5 mr-2" /> Logs
+                </DrawerTitle>
+                <ScrollArea className="h-[400px] w-full rounded-md border p-4 bg-black text-cyan-500 font-mono">
+                  <pre>
+                    <code>{terminal_code}</code>
+                  </pre>
+                </ScrollArea>
+              </DrawerHeader>
+              <DrawerFooter>
+                <DrawerClose>
+                  <Button variant="outline">Close Logs</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </div>
+      </main>
+    </div>
   );
 }
